78d623853584885790cf1d5e765ab3881d559156
Henrik Rexed <henrik.rexed@dynatrace.com>
2025-10-14 20:32:21 +0400

    update node port for kgateway: istio and amgient-kgateway

--- deployment.sh
+++ deployment.sh
@@ -271,9 +271,9 @@ else
     kubedtl apply -f linkerd/referencegrant.yaml
     kubect apply -f linkerd/observability.yaml
     echo "🔧 Applying patch..."
-    HTTP_IDX=$(kubectl get svc bookinfo-gateway-kgateway -n booking -o json |  jq -r '.spec.ports | to_entries | .[] | select(.value.name == "http") | .key')
+    HTTP_IDX=$(kubectl get svc bookinfo-gateway -n booking -o json |  jq -r '.spec.ports | to_entries | .[] | select(.value.name == "http") | .key')
     PATCH_OPS="[{\"op\": \"replace\", \"path\": \"/spec/ports/${HTTP_IDX}/nodePort\", \"value\": 30080}]"
-    kubectl patch svc bookinfo-gateway-kgateway -n booking  --type='json'  -p="${PATCH_OPS}"
+    kubectl patch svc bookinfo-gateway -n booking  --type='json'  -p="${PATCH_OPS}"
 
   else
      if [  "$TYPE" = 'ambient' ]; then
@@ -319,9 +319,9 @@ else
                kubectl apply -f opentelemetry/openTelemetry-manifest_statefulset_kgateway.yaml
                kubectl apply -f kgateway-ambient/referencegrant.yaml
                echo "🔧 Applying patch..."
-               HTTP_IDX=$(kubectl get svc bookinfo-gateway-kgateway -n kgateway-system -o json |  jq -r '.spec.ports | to_entries | .[] | select(.value.name == "http") | .key')
+               HTTP_IDX=$(kubectl get svc bookinfo-gateway -n kgateway-system -o json |  jq -r '.spec.ports | to_entries | .[] | select(.value.name == "http") | .key')
                PATCH_OPS="[{\"op\": \"replace\", \"path\": \"/spec/ports/${HTTP_IDX}/nodePort\", \"value\": 30080}]"
-               kubectl patch svc bookinfo-gateway-kgateway -n kgateway-system  --type='json'  -p="${PATCH_OPS}"
+               kubectl patch svc bookinfo-gateway -n kgateway-system  --type='json'  -p="${PATCH_OPS}"
 
 
           else
